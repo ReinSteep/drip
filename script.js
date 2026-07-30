c2ce4f8b7c2c6927dfac4aa7c0aad72709e29d12
ReinSteep <filip.stowasser@gmail.com>
2026-07-30 11:56:58 +0200

    Load first product data

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ async function loadProductList() {
   const response = await fetch("products.json");
   const productList = await response.json();
 
-  console.log(productList);
+  const firstProductFolder = productList[0].folder;
+
+  const productResponse = await fetch(
+    `items/${encodeURIComponent(firstProductFolder)}/info.json`
+  );
+
+  const productInfo = await productResponse.json();
+
+  console.log(productInfo);
 }
 
 loadProductList();
