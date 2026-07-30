c089803c34fec7b39318829d5c4cb816f176a22b
ReinSteep <filip.stowasser@gmail.com>
2026-07-30 11:42:12 +0200

    Load product list

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Drip loaded");
+async function loadProductList() {
+  const response = await fetch("products.json");
+  const productList = await response.json();
+
+  console.log(productList);
+}
+
+loadProductList();
